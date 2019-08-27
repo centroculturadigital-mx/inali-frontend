559e0393982e0ffacd5492b414dfb6aa91916dea
furenku <furenku@gmail.com>
2019-08-26 21:29:25 -0500

    docker para entorno local

--- src/App.js
+++ src/App.js
@@ -9,22 +9,9 @@ const App = () => {
 
   return (
     <div className="App">
-
-      <FamiliasLista />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        inali-frontend
+      </h1>
     </div>
   );
 }

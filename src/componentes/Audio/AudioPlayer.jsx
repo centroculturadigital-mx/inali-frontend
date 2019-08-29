d3bbbaa036c6d851519024a9227419d2823892ff
josecaos <jazzvoon@gmail.com>
2019-08-29 17:35:13 -0500

    Avances + debug player personalizado

--- src/componentes/Audio/AudioPlayer.jsx
+++ src/componentes/Audio/AudioPlayer.jsx
@@ -12,7 +12,6 @@ export default class AudioPlayer extends React.Component {
 
   render() {
     return (
-
       <div>
         <audio id="AudioPlayer" controls>
           <source src={mp3} type="audio/mpeg" />
@@ -20,52 +19,69 @@ export default class AudioPlayer extends React.Component {
           El navegador no soporta HTML5
         </audio>
       </div>
-
     );
   }
 }
-
+//player custom
 export class JPlayer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+constructor(props) {
+  super(props)
+  this.props = this.props
+}
 
+playerHTML() {
+  const audioTag = document.createElement('audio')
+  const audioContext = new AudioContext()//crea contexto de audio
+  const track = audioContext.createMediaElementSource(audioTag)// pasa el tag al contexto de audio
+  const playButton = document.querySelector('button')
 
-  audioScripts() {
-    const audioContext = new AudioContext()//cra contexto de audio
-    const audioElement = document.querySelector('.JPlayer audio')//instancia el tag audio
-    const track = audioContext.createMediaElementSource(audioElement)// pasa el tag al contexto de audio
+  audioTag.setAttribute('src', mp3,)//agrega atributo y pasa el audio
+  audioTag.setAttribute('type','audio/mpeg')
+  audioTag.setAttribute('className','JPlayerTrack')
+  track.connect(audioContext.destination)
 
-    return (String(audioContext) + ": " + JSON.stringify(audioContext))
-  }
+// // play event
+// playButton.addEventListener('click', function() {
+// //
+// // revisa si se encuentra en estado de suspencion (autoplay policy)
+// if (audioContext.state === 'suspended') {
+// audioContext.resume();
+// }
+// // play or pause dependiendo del estado
+// if (this.dataset.playing === 'false') {
+// audioTag.play();
+// this.dataset.playing = 'true';
+// } else if (this.dataset.playing === 'true') {
+//   audioTag.pause();
+//   this.dataset.playing = 'false';
+// }
+// }, false)
+console.log(playButton)
 
-  playerHTML() {
-    return (
-      <section>
-        <h1>Player custom</h1>
-        <div className="JPlayer">
-          <audio src={ogg} type="audio/mpeg"></audio>
-          {/* play/pause: */}
-          <span id="PlayToggle" />
-          {/*  track: */}
-          <span id="Timeline">
-            <span id="Loading" />
-            <span id="Handle" className="ui-slider-handle" />
-          </span>
-          <span id="TimeLeft" />
-          {this.audioScripts()}
-        </div>
-      </section>
-    )
-  }
-
-
-  //
-  render() {
   return (
-    this.playerHTML()
-  );
+    <section>
+      <h1>Player custom</h1>
+      <div className="JPlayer">
+        {/* play/pause: */}
+        <button data-playing="false" role="switch" aria-checked="false">
+          <span>Play/Pause</span>
+        </button>
+        {/*  track: */}
+        <span id="Timeline">
+          <span id="Loading" />
+          <span id="Handle" className="ui-slider-handle" />
+        </span>
+        <span id="TimeLeft" />
+      </div>
+    </section>
+  )
+}
+
+
+//
+render() {
+return (
+  this.playerHTML()
+);
 }
 }
